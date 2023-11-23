ae46a48ef205ec43172e3e8e4673b62b197e42d9
Meirasulin <asulinmeir64@gmail.com>
2023-11-23 10:18:54 +0200

    update

--- src/auth/controller/authController.ts
+++ src/auth/controller/authController.ts
@@ -7,10 +7,8 @@ import { handleError } from "../../utils/handleErrors";
 export const handleUserRegistration = async (req: Request, res: Response) => {
   try {
     const user: UserInterface = req.body;
-
     const { error } = userValidation(user);
     if (error?.details[0].message) throw new Error(error?.details[0].message);
-
     const userFromDB = await register(user);
     return res.status(201).send(userFromDB);
   } catch (error) {
