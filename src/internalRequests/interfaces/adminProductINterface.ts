d7052b5bff343aaafbd5d73e93db973cea54ae10
Meirasulin <asulinmeir64@gmail.com>
2023-11-27 12:41:27 +0200

    bugs fix

--- src/internalRequests/interfaces/adminProductINterface.ts
+++ src/internalRequests/interfaces/adminProductINterface.ts
@@ -4,5 +4,5 @@ export interface AdminProductInterface extends ShopProductInterface {
   isForSale: boolean;
   costPrice: number;
   supplier: string;
-  createdBy: string;
+  createdBy?: string;
 }
