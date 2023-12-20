5effcedfe502ec073a33613135c4853a981223fa
yakovperets <141735529+yakovperets@users.noreply.github.com>
2023-12-20 15:26:57 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+// jest.config.js
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   globals: {
     'ts-jest': {
-      tsconfig: 'tsconfig.test.json'
-    }
-  }
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
+  moduleNameMapper: {
+    // Add any necessary module name mappings for non-JS modules here
+    // For example, if you have a CSS file: '\\.css$': '<rootDir>/path/to/styleMock.js'
+  },
 };
